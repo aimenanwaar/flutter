a1383982f71b66952620718ef06b8193a8a29d9d
aimenanwaar <aimenanwaar13@gmail.com>
2022-01-19 14:20:53 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -33,103 +33,161 @@ class _MainPageState extends State<MainPage> {
 
   Map<String, dynamic>? str1;
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        // backgroundColor: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.blueGrey,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          elevation: 15,
+          centerTitle: true,
+        ),
+      ),
       title: 'Flutter API Project',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('API Project'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if(currentPosition != null ) Text(
-                  "LONGITUDE: ${currentPosition!.longitude}"
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              if(currentPosition != null ) Text(
-                  "LATITUDE: ${currentPosition!.latitude}"
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              TextButton(
-                onPressed: () async {
-                  currentPosition = getCurrentLocation();
-                },
-                child: const Text('Get Location'),
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.all(20),
-                  primary: Colors.white,
-                  backgroundColor: Colors.black,
-                  textStyle: const TextStyle(
-                    fontSize: 20,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () async {
+                      currentPosition = getCurrentLocation();
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(Icons.add_location),
+                        SizedBox(width: 10,),
+                        Text('Get Location'),
+                      ],
+                    ),
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(15),
+                      primary: Colors.white,
+                      backgroundColor: Colors.black,
+                      textStyle: const TextStyle(
+                        fontSize: 23,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              if(str1 != null ) Text(
-                'City: ${str1!['name']}',
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              if(str1 != null ) Text(
-                'Temp: ${str1!['main']['temp']} °C',
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              if(str1 != null ) Text(
-                'Feels Like: ${str1!['main']['feels_like']} °C',
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              if(str1 != null ) Text(
-                'Min Temp: ${str1!['main']['temp_min']} °C',
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              if(str1 != null ) Text(
-                'Max Temp: ${str1!['main']['temp_max']} °C',
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              if(str1 != null ) Text(
-                'Visibility: ${str1!['visibility']}',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextButton(
-                onPressed: () async {
-                  str = await getWeather(longitude: currentPosition!.longitude, latitude: currentPosition!.latitude);
-                  str1 = jsonDecode(str!);
-                  setState(() { });
-                },
-                child: const Text('Get Weather'),
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.all(20),
-                  primary: Colors.white,
-                  backgroundColor: Colors.black,
-                  textStyle: const TextStyle(
-                    fontSize: 20,
+                  TextButton(
+                    onPressed: () async {
+                      str = await getWeather(longitude: currentPosition!.longitude, latitude: currentPosition!.latitude);
+                      str1 = jsonDecode(str!);
+                      setState(() { });
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(Icons.cloud),
+                        SizedBox(width: 10,),
+                        Text('Get Weather'),
+                      ],
+                    ),
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(15),
+                      primary: Colors.white,
+                      backgroundColor: Colors.black,
+                      textStyle: const TextStyle(
+                        fontSize: 23,
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  if(currentPosition != null ) const SizedBox(
+                    height: 10,
+                  ),
+                  if(currentPosition != null ) Text(
+                    "LONGITUDE: ${currentPosition!.longitude}",
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  if(currentPosition != null ) const SizedBox(
+                    height: 5,
+                  ),
+                  if(currentPosition != null ) Text(
+                    "LATITUDE: ${currentPosition!.latitude}",
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  if(str1 != null ) const SizedBox(
+                    height: 10,
+                  ),
+                  if(str1 != null ) Text(
+                    'Current weather in ${str1!['name']} is: ',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  if(str1 != null ) const SizedBox(
+                    height: 5,
+                  ),
+                  if(str1 != null ) Text(
+                    '${str1!['main']['temp']} °C',
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  if(str1 != null ) const SizedBox(
+                    height: 10,
+                  ),
+                  if(str1 != null ) Text(
+                    'Feels Like: ${str1!['main']['feels_like']} °C',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  if(str1 != null ) const SizedBox(
+                    height: 5,
+                  ),
+                  if(str1 != null ) Text(
+                    'Description: ${str1!['weather'][0]['description']}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  if(str1 != null ) const SizedBox(
+                    height: 5,
+                  ),
+                  if(str1 != null ) Text(
+                    'Visibility: ${str1!['visibility']}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
